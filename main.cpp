cc17eef56e1cf0d4002ed9e317ea8de6bfb2fbd8
Alan King <kingaj@us.ibm.com>
2019-03-04 16:35:47 +0000

    added newsvendor.json

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
+//
+// Created by kingaj on 2/27/19.
+//
+
+#include <cstdio>
 #include <iostream>
+#include <exception>
+
+#include <rapidjson/filereadstream.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/document.h>
+
+using namespace rapidjson;
+
+
+#define TEST_JSON_FILE "../json/v2-newsvendor/newsvendor.json"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    try {
+        FILE *fp = fopen(TEST_JSON_FILE, "r");
+        if (!fp) {
+            std::cerr << "File " << TEST_JSON_FILE << " not found" << std::endl;
+            throw std::exception();
+        }
+        char readBuffer[65536];
+        FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+        Document d;
+        d.ParseStream(is);
+        fclose(fp);
+
+        StringBuffer buffer;
+
+        buffer.Clear();
+
+        PrettyWriter<StringBuffer> writer(buffer);
+        d.Accept(writer);
+
+
+        std::cout << buffer.GetString() << std::endl;
+        return 0;
+    }
+    catch (std::exception &e) {
+        return 1;
+    }
 }
